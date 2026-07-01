f29c2ab6bef70327c9e5726d07c3a31fa4279847
Mayank Singh <mayankrajeevsingh@gmail.com>
2026-07-02 00:20:32 +0530

    Added solution - LeetHub

---
+++ 703289-segregate-0s-and-1s-GFG/703289-segregate-0s-and-1s.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    void segregate0and1(vector<int> &arr) {
+        // code here
+        // int left =0;
+        // int right = arr.size()-1;
+        
+        // while(left<right){
+        //     if()
+        // }
+        sort(arr.begin(),arr.end());
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
