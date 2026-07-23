5cf0c12191cd9db8e58719de85f6957eb1cc0e87
Mayank Singh <mayankrajeevsingh@gmail.com>
2026-07-23 22:58:40 +0530

    Time: 0 ms (100%), Space: 198.7 MB (98.13%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        int n = nums.size();
+
+        if (n <= 2)
+            return n;
+
+        int bits = 32 - __builtin_clz(n);
+
+        return 1 << bits;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
