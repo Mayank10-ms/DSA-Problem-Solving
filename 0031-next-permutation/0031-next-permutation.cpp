98b8b5d14aef7ed87efb01f0687cc9883000a000
Mayank Singh <mayankrajeevsingh@gmail.com>
2026-08-26 00:01:58 +0530

    Time: 0 ms (100%), Space: 15.9 MB (15.15%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        next_permutation(nums.begin(),nums.end());
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
