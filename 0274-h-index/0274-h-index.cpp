15bbc22a21d7fb9caee0b964cd5933509cf4cd6b
Mayank Singh <mayankrajeevsingh@gmail.com>
2026-08-29 13:07:55 +0530

    Time: 0 ms (100%), Space: 12.3 MB (35.1%) - LeetHub - Time: O(n log n), Space: O(1)

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+      sort(citations.begin(),citations.end(),greater<int>());
+        int n = citations.size();
+        int H =0;
+        for(int i =0;i<n;i++){
+            if(citations[i]>=i+1){
+                H=i+1;
+            }
+            else{
+                break;
+            }
+        }
+        return H;
+        
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
