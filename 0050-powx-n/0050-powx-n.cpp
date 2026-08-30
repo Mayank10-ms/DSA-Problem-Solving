358db77129ab25c36e5e1aadf0bbc9a76d91b070
Mayank Singh <mayankrajeevsingh@gmail.com>
2026-08-30 08:53:09 +0530

    Time: 0 ms (100%), Space: 8.8 MB (7.94%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans = 0;
+        ans = pow(x,n);
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
