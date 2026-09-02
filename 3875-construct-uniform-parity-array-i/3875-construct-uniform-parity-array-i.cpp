cf18930959bf6da68cda7bf88c42df032323629f
Mayank Singh <mayankrajeevsingh@gmail.com>
2026-09-02 06:53:35 +0530

    Time: 0 ms (100%), Space: 30.2 MB (67.8%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+    //     int n = nums1.size();
+    //     vector<int>nums2(n);
+    //     for(int i = 0;i<n;i++){
+    //         for(int j=i+1;j<n;j++){
+    //       nums2[i]=nums1[i]-nums1[j];
+    //       nums2[i]=nums1[i];
+    //       if(nums2[i]%2==0) return true;
+    //     }
+    // }
+    //     return false;
+    return true;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
