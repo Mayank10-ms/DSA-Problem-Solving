675431c6e69a9cb1ca1c5a0fe11610ee376164ed
Mayank Singh <mayankrajeevsingh@gmail.com>
2026-09-05 14:48:12 +0530

    Time: 0 ms (100%), Space: 8.3 MB (84.83%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string makeGood(string s) {
+       string result;
+       for(char &ch : s){
+        if( !result.empty() && (result.back()+32 == ch || result.back()-32 == ch)){
+            result.pop_back();
+        }
+        else{
+            result.push_back(ch);
+        }
+       } 
+       return result;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
