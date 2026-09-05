5d17c3a44a78cb66d60292fcd5a99128d1828729
Mayank Singh <mayankrajeevsingh@gmail.com>
2026-09-05 10:52:40 +0530

    Time: 1 ms (73.37%), Space: 15.6 MB (15.29%) - LeetHub - Time: O(n * k), Space: O(1)

---
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int n = strs.size();
+        int k = strs[0].size();
+        int count=0;
+
+        for(int i =0;i<k;i++){
+            for(int j =1;j<n;j++){
+                if(strs[j][i]<strs[j-1][i]) {
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
