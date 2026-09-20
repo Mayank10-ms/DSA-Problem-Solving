1236118df8f95b2cfe387c8a2db29aedaab4d47f
Mayank Singh <mayankrajeevsingh@gmail.com>
2026-09-20 16:26:49 +0530

    Time: 0 ms (100%), Space: 9.7 MB (47.88%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int sum = 0;
+        for(int i =0;i<s.size();i++){
+           int rev = 'z'-s[i]+1;
+           sum+=rev*(i+1);
+    }
+    return sum;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
