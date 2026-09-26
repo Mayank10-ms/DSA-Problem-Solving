3900a26cb17f2e7dede43100387c7fdc4a8385e6
Mayank Singh <mayankrajeevsingh@gmail.com>
2026-09-26 22:10:34 +0530

    Time: 0 ms (100%), Space: 50.5 MB (44.55%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 4034-minimum-bishop-moves-to-reach-target/4034-minimum-bishop-moves-to-reach-target.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minBishopMoves(vector<int>& source, vector<int>& target) {
+        int sr = source[0];
+        int sc = source[1];
+        int tr = target[0];
+        int tc = target[1];
+
+        if(sr==tr and sc == tc) {
+            return 0;
+        }
+        
+        if(abs(sr-tr)==abs(sc-tc)){
+            return 1;
+        }
+        if((sr+sc)%2 == (tr+tc)%2){
+            return 2;
+        }
+        return -1;
+
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
